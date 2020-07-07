6912e286cdc46dc5baaa9a731c8f4d86c74bf1ff
eatdreamcat <meow.std@outlook.com>
2020-07-07 12:25:21 +0800

    ssssss

--- DaubCash/build/jsb-link/main.js
+++ DaubCash/build/jsb-link/main.js
@@ -173,7 +173,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.3c21e.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.385a6.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -209,7 +209,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.91f12.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.8853c.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -235,12 +235,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.9ca00.js');
+        require('src/settings.116ee.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.9ca00.js');
+        require('src/settings.116ee.js');
         require('src/cocos2d-jsb.2b130.js');
         require('jsb-adapter/jsb-engine.js');
     }
