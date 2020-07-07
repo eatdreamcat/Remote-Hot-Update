e33514d869662a10dc008387090d603b5babdaa9
eatdreamcat <meow.std@outlook.com>
2020-07-07 19:16:45 +0800

    1.2.7

--- DaubCash/build/jsb-link/main.js
+++ DaubCash/build/jsb-link/main.js
@@ -13,9 +13,10 @@
             var storagePath = paths[0] || '';
             var tempPath = storagePath + '_temp/';
             var baseOffset = tempPath.length;
-
+            console.log("tempPath:", tempPath);
             if (jsb.fileUtils.isDirectoryExist(tempPath) && !jsb.fileUtils.isFileExist(tempPath + 'project.manifest.temp')) {
                 jsb.fileUtils.listFilesRecursively(tempPath, fileList);
+                console.log("fileList:", fileList);
                 fileList.forEach(srcPath => {
                     var relativePath = srcPath.substr(baseOffset);
                     var dstPath = storagePath + relativePath;
@@ -175,7 +176,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.860fe.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8bfea.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -211,7 +212,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.baba7.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.fe02d.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -237,12 +238,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.9f5fb.js');
+        require('src/settings.f7052.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.9f5fb.js');
+        require('src/settings.f7052.js');
         require('src/cocos2d-jsb.379b3.js');
         require('jsb-adapter/jsb-engine.js');
     }
